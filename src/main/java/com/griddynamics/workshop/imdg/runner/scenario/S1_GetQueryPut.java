5f576a38b4e2a6c4c49aedde2735322c041db5a7
Maxim <desliner@gmail.com>
2013-10-15 12:27:16 +0300

    minor changes

--- src/main/java/com/griddynamics/workshop/imdg/runner/scenario/S1_GetQueryPut.java
+++ src/main/java/com/griddynamics/workshop/imdg/runner/scenario/S1_GetQueryPut.java
@@ -7,8 +7,8 @@ import com.tangosol.net.CacheFactory;
 import com.tangosol.net.NamedCache;
 import com.tangosol.util.filter.EqualsFilter;
 
-import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author mmyslyvtsev@griddynamics.com
@@ -27,7 +27,7 @@ public class S1_GetQueryPut extends Client {
         User owner = (User) usersCache.get(post.getOwnerUserId());
         log.info("Owner: {}", owner);
 
-        Collection<Map.Entry<Integer, User>> users = usersCache.entrySet(new EqualsFilter(User.EXTRACTOR_AGE, owner.getAge()));
+        Set<Map.Entry<Integer, User>> users = usersCache.entrySet(new EqualsFilter(User.EXTRACTOR_AGE, owner.getAge()));
         log.info("Number of users with same age ({}): {}", owner.getAge(), users.size());
 
         int newUpVotes = owner.getUpVotes() + users.size();
